f8e9276ad4ba446d3af9e8f65831d7cdf4dcad5d
Gregory Osorio <e1977078@cmaisonneuve.qc.ca>
2023-01-27 10:22:14 -0500

    s1c2 Ajout du readme.md

--- index.php
+++ index.php
@@ -5,6 +5,17 @@
      */
 
      get_header();
-     get_footer();
 ?> 
 <h1>Bienvenue dans le cours 4W4</h1>
+
+<main>
+    <?php if(have_posts()):
+            while(have_posts()) : the_post();
+            the_title('<h1>','<h1/>');
+            the_content(); ?>
+            <hr>
+    <?php endwhile;
+    endif; ?>  ?>  
+</main>
+
+<?php get_footer(); ?> 
