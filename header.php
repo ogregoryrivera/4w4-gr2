9b0a4c6944b82d378b06df151da6ee525ee15345
Gregory Osorio <e1977078@cmaisonneuve.qc.ca>
2023-02-06 12:54:59 -0500

    s3c1 ajout composant menu_entete

--- header.php
+++ header.php
@@ -1,7 +1,3 @@
-<?php 
-    wp_head();
-?> 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,11 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php 
+    wp_head();
+?> 
 </head>
 <body>
     <header>
         <?php wp_nav_menu(array(
-            "menu" => "entete"
+            "menu" => "entete",
+            "container" => "nav"
         )) ?> 
         <h1><a href="<?= bloginfo('url') ?>"><?= bloginfo('name') ?></a></h1>
         <h2><?= bloginfo('description') ?></h2>
