fc02ce806ee511f951513d0f10f3048e5eed397e
Gregory Osorio <e1977078@cmaisonneuve.qc.ca>
2023-05-23 14:34:19 -0400

    Animation des pages template-atelier

--- template-parts/template-atelier.php
+++ template-parts/template-atelier.php
@@ -11,11 +11,11 @@ if ( have_posts() ) : the_post(); ?>
 <?php the_post_thumbnail('thumbnail'); ?> 
 <h1><?= get_the_title(); ?></h1>
 <?php the_content();?>
-<p>Formateur: <?php the_field('formateur'); ?></p>
-<p>Date de début de l’atelier: <?php the_field('date'); ?></p>     
-<p>Heure de début <?php the_field('heure'); ?></p>     
-<p>Local ou se déroulera l’atelier: <?php the_field('local'); ?></p>     
-<p> <?php the_field('date'); ?></p>     
+<p class="atelier-box">Formateur: <?php the_field('formateur'); ?></p>
+<p class="atelier-box">Date de début de l’atelier: <?php the_field('date'); ?></p>     
+<p class="atelier-box">Heure de début <?php the_field('heure'); ?></p>    
+<p class="atelier-box">Durée du cours: <?php the_field('duree'); ?>h</p>  
+<p class="atelier-box">Local ou se déroulera l’atelier: <?php the_field('local'); ?></p> 
 <?php endif;?>
 </main><!-- #main -->
 <?php
